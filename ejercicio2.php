358b7a1e7c73d47dcb03e1759395da5e18f001aa
irbin verona olivares <Irbing@Cliente12.GRANJASELPOLLON.COM.MX>
2024-12-03 22:44:29 -0600

    ejercicio2

---
+++ ejercicio2.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<?php
+
+$edad = 20;
+$peso = 90.25;
+$nombre = "juan";
+$existe = true;
+echo"variable integer ";
+echo $edad;
+echo "variable double ";
+echo $peso;
+echo "<br>";
+echo "variable string ";
+echo $nombre;
+echo "<br>";
+echo "variable boolean ";
+echo $existe;
+
+
+?>
+</body>
+
+    </html>
