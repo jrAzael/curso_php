8a39707735a4e641a20f4a3b6eff8d5cede71a69
irbin verona olivares <Irbing@Cliente12.GRANJASELPOLLON.COM.MX>
2024-12-03 22:55:12 -0600

    ejercicio4

---
+++ ejercicio4.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+    <?php
+   $valor = rand(1,100);
+   $inicio = 1;
+   while($inicio <= $valor) {
+    echo $inicio;
+    echo "<br>";
+    $inicio++;
+   }
+    ?>
+</body>
+</html>
