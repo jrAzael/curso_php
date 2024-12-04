d9083dc46f02aee11bcf79b89109af111140a072
irbin verona olivares <Irbing@Cliente12.GRANJASELPOLLON.COM.MX>
2024-12-03 22:57:35 -0600

    ejercicio5

---
+++ ejercicio5.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+    
+ <form action="pagina2.php" method="post">
+ Ingrese el primer valor:
+ <input type ="text" name="valor1">
+ <br>
+ingrese segundo valor:
+<input type ="text" name="valor2">
+<br>
+<input type="radio" name="radio1" value="suma">sumar
+<br>
+<input type="radio" name ="radio1" value="resta">restar
+<br>
+<input type="submit" name ="operar"> 
+    
+</body>
+</html>
