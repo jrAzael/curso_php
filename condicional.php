ab257d076d07eeac488e320de3d0486df5573821
irbin verona olivares <Irbing@Cliente12.GRANJASELPOLLON.COM.MX>
2024-12-04 22:12:21 -0600

    condicionales

---
+++ condicional.php
@@ -0,0 +1,23 @@
+
+<html>
+
+<head>
+<title>problema</title>
+</head>
+<body>
+<?php
+$valor = rand(1,10);
+echo "el valor sorteado es $valor<br>";
+if ($valor <= 5) {
+    echo "Es menor o igual a 5";
+}else {
+    echo "es mayor a 5";
+}
+
+?>
+</body>
+<body>
+    <?php
+    ?>
+</body>
+    </html>
